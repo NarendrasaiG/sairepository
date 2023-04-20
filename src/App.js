fb9f330cc3943739427c177c03dce755fbac7c94
manjunadhb <manjunadhb@gmail.com>
2023-04-20 16:28:34 +0530

    modified by manjunadh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>Narendra Sai is Good boy</h1>
-    <h1>Syam is Class toper</h1>
-    <h1>Srujan is Handsome boy </h1>
+      <h1>Narendra Sai is Good boy</h1>
+      <h1>Syam is Class toper</h1>
+      <h1>Srujan is Handsome boy </h1>
+      <h1>This line is added by Manjunadh</h1>
     </div>
   );
 }
